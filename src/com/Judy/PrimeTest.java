56bc645ce7f52dbe599559741ff5eb4c8b90d48e
Judy <aju452488@gmail.com>
2017-11-13 20:30:56 +0800

    Answer

--- src/com/Judy/PrimeTest.java
+++ src/com/Judy/PrimeTest.java
@@ -1,24 +1,23 @@
 package com.Judy;
 
-//搞不懂啊啊啊啊啊(崩潰
+//搞懂了耶!
 
 public class PrimeTest {
 
 	public static void main(String[] args) {
-		
-		int n = 7;
+
+		int n = 2;
 		boolean prime = true;
 		for (int i = 2; i < n; i++) {
-			
+
 			if (n % i == 0) {
 				prime = false;
 				break;
 			}
-		
-			if (prime) {
-				System.out.println(n);
-				
-			}
+
+		}
+		if (prime) {
+			System.out.println(n);
 
 		}
 
