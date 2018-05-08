0ff9083a92d140927e0fe50f6b909783babae3de
Judy <aju452488@gmail.com>
2018-05-08 15:25:03 +0800

    Main系列3

--- src/com/java2/collection/Main.java
+++ src/com/java2/collection/Main.java
@@ -4,24 +4,34 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.naming.StringRefAddr;
+
 public class Main {
 
 	public static void main(String[] args) {
-//		<法一>
-//        List<Integer> list = new ArrayList<>();
-//        list.add(2);
-//        list.add(0);
-//        list.add(1);
-//        list.add(7);
+		// <法一>
+		// List<Integer> list = new ArrayList<>();
+		// list.add(2);
+		// list.add(0);
+		// list.add(1);
+		// list.add(7);
 
-//		<法二> 我忘記了(qaq),但又找到了(ovo)
-		List<Integer> list= new ArrayList<>(Arrays.asList(2,0,1,7));
-	    System.out.println(list);
+		// <法二> 我忘記了(qaq),但又找到了(ovo)
+		List<Integer> list = new ArrayList<>(Arrays.asList(2, 0, 1, 7));
+		System.out.println(list);
+//      第二個   		
+//		List<String> nameList = new ArrayList<>(Arrays.asList("Mr.Green", "Mr.Yellow", "Mr.Red"));
+
+//		for (String name : nameList) {
+//			System.out.println(name);
+//		}
 		
 		List<String> nameList = new ArrayList<>(Arrays.asList("Mr.Green", "Mr.Yellow", "Mr.Red"));
-		
-	    for(String name:nameList){
-	    	System.out.println(name);
+
+		String[] strArray = nameList.toArray(new String[0]);
+
+	    for (int i = 0; i < strArray.length; i++) {
+	      System.out.println(strArray[i]);
 	    }
 
 
