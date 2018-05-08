b5e688b1d2760beb3514c0bd531bb694d4529ba8
Judy <aju452488@gmail.com>
2018-05-08 15:13:45 +0800

    Main系列1

---
+++ src/com/java2/collection/Main.java
@@ -0,0 +1,24 @@
+package com.java2.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		<法一>
+//        List<Integer> list = new ArrayList<>();
+//        list.add(2);
+//        list.add(0);
+//        list.add(1);
+//        list.add(7);
+
+//		<法二> 我忘記了(qaq),但又找到了(ovo)
+		List<Integer> list= new ArrayList<>(Arrays.asList(2,0,1,7));
+	    System.out.println(list);
+
+
+	}
+
+}
