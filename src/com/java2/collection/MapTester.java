0ed80e0eb5119f35e4018b293cfc457789f1d1b7
Judy <aju452488@gmail.com>
2018-05-14 19:09:27 +0800

    Map系列2

--- src/com/java2/collection/MapTester.java
+++ src/com/java2/collection/MapTester.java
@@ -1,17 +1,28 @@
 package com.java2.collection;
 
+import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class MapTester {
 
 	public static void main(String[] args) {
-    SortedMap<String, Integer> name =new TreeMap<>();
-    name.put("Omega", 24);
-    name.put("Alpha", 1);
-    name.put("Gamma", 3);
+//		Map第一題
+//    SortedMap<String, Integer> map =new TreeMap<>();
+//    map.put("Omega", 24);
+//    map.put("Alpha", 1);
+//    map.put("Gamma", 3);
 
-    System.out.print(name);
-	}
-
-}
+//    System.out.println(map);
+		
+//		Map第二題,不知道為什麼不行過??
+		Map<String, Integer> map = new TreeMap<>();
+	    map.put("Gamma",  3);
+	    map.put("Omega", 24);
+	    map.put("Alpha",  1);
+	    
+	    for(Map.Entry<String, Integer> m: map.entrySet()) {
+	    	System.out.println(m);
+	    }
+	    }
+	    }
