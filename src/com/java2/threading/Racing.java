ebd1642f79d35f059c547536bee85cd1f85c8dcc
Judy <aju452488@gmail.com>
2018-05-15 16:10:28 +0800

    Racing-join

--- src/com/java2/threading/Racing.java
+++ src/com/java2/threading/Racing.java
@@ -27,7 +27,17 @@ public class Racing {
 		HorseRunnable h2 = new HorseRunnable();
 		Thread thr = new Thread(h2);
 		thr.start();
-
+		
+//		join是等待,如此一來,Main ended才會在兩隻馬都跑完後才印出
+        try {
+			h1.join();
+			thr.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        System.out.println("Main ended");
+        
 	}
 
 }
