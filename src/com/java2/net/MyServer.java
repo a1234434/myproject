39fda67e356014d705516ec3753b71ef7bf3643f
Judy <aju452488@gmail.com>
2018-05-29 16:29:13 +0800

    Socket系列

---
+++ src/com/java2/net/MyServer.java
@@ -0,0 +1,44 @@
+package com.java2.net;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javax.swing.JFrame;
+
+public class MyServer extends JFrame{
+       public MyServer(){
+    	   setSize(600,200);
+    	   setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    	   setVisible(true);
+       }
+	public void start() {
+		try {
+			ServerSocket server =new ServerSocket();
+			Socket socket =server.accept();
+			System.out.println("Connected");
+			InputStream is =socket.getInputStream();
+			InputStreamReader isr =new InputStreamReader(is);
+			BufferedReader br =new BufferedReader(isr);
+			String line = br.readLine();
+			System.out.println(line);
+			String token[] =line.split(":");
+			String command =token[0];
+			String option =token[1];
+			System.out.println(command+"/"+option);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	public static void main(String[] args) {
+        
+        
+	}
+
+}
