7b5554114c1a29624cc2c9dc717713ddfb49d545
Judy <aju452488@gmail.com>
2018-06-25 17:06:54 +0800

    MapTestPractice2

--- src/com/java2/collection/MapTestPractice.java
+++ src/com/java2/collection/MapTestPractice.java
@@ -1,5 +1,6 @@
 package com.java2.collection;
 
+import java.util.Map.Entry;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -11,8 +12,18 @@ public class MapTestPractice {
 		map.put("Omega", 24);
 		map.put("Alpha", 1);
 		map.put("Gamma", 3);
-		
-		System.out.println(map);
+/* 網路上找到的
+ for (Entry<StringBuilder, Integer> entry : wordCountMap.entrySet()) {
+	    String key = entry.getKey().toString();
+	    Integer value = entry.getValue();
+	    System.out.println("key, " + key + " value " + value);
+	}		
+	*/
+		for(Entry<String, Integer> entry :map.entrySet()) {
+			String name =entry.getKey().toString();
+			Integer value =entry.getValue();
+			System.out.println(name+"="+value);
+		}
 	}
 
 }
