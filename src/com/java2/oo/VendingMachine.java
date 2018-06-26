4f57ca7760cccdec8f7b66ef70ca8401fa0ec99c
Judy <aju452488@gmail.com>
2018-06-26 08:20:43 +0800

    VM-practice-finish

--- src/com/java2/oo/VendingMachine.java
+++ src/com/java2/oo/VendingMachine.java
@@ -13,19 +13,21 @@ public class VendingMachine {
 	List<Drink> drinks = new ArrayList<>();
 
 	public void On() {
-		System.out.println("請投入金幣(5,10,50)或選擇飲料(1,2,3),或輸入0結束:");
+		
+//		for (int i = 0; i < drinks.size(); i++) {
+//			Drink drink = drinks.get(i);
+//			System.out.println(drink.getId() + "\t" + drink.getName() + "\t" + drink.getPrice());
+//		}
+		System.out.println("販賣機餘額:" + total);
+		int data = -1;
+		while (data != 0) {
+		System.out.print("請投入金幣(5,10,50)或選擇飲料(1,2,3),或輸入0結束:");
 		Scanner s = new Scanner(System.in);
 		int p = Integer.parseInt(s.nextLine());
-		for (int i = 0; i < drinks.size(); i++) {
-			Drink drink = drinks.get(i);
-			System.out.println(drink.getId() + "\t" + drink.getName() + "\t" + drink.getPrice());
-		}
-		int data = -1;
-		System.out.println("販賣機餘額:" + total);
-		while (data != 0) {
-			data = p;
-			switch (data) {
+			switch (p) {
 			case 0:
+				data=0;
+				System.out.println("謝謝惠顧");
 				break;
 			case 5:
 				total = total + 5;
@@ -41,9 +43,10 @@ public class VendingMachine {
 				break;
 			case 1:
 				if (total - a >= 0) {
-					System.out.println("DON!");
+					Drink drink1 =drinks.get(0);
+					System.out.println("DON!"+"你的飲料:"+drink1.getName());
+					total = total - drink1.getPrice();
 					System.out.println("販賣機餘額:" + total);
-					total = total - a;
 				} else {
 					System.out.println("BEEP!");
 					System.out.println("販賣機餘額:" + total);
@@ -51,9 +54,10 @@ public class VendingMachine {
 				break;
 			case 2:
 				if (total - b >= 0) {
-					System.out.println("DON!");
+					Drink drink2 = drinks.get(1);
+					System.out.println("DON!"+"你的飲料:"+drink2.getName());
+					total = total - drink2.getPrice();
 					System.out.println("販賣機餘額:" + total);
-					total = total - b;
 				} else {
 					System.out.println("BEEP!");
 					System.out.println("販賣機餘額:" + total);
@@ -61,9 +65,10 @@ public class VendingMachine {
 				break;
 			case 3:
 				if (total - c >= 0) {
-					System.out.println("DON!");
+					Drink drink3 =drinks.get(2);
+					System.out.println("DON!"+"你的飲料:"+drink3.getName());
+					total = total - drink3.getPrice();
 					System.out.println("販賣機餘額:" + total);
-					total = total - c;
 				} else {
 					System.out.println("BEEP!");
 					System.out.println("販賣機餘額:" + total);
